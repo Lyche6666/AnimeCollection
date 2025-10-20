6d95427cff132ee2ac0c0bcafc6a25057cb99303
Lyche <caroxmeme@gmail.com>
2025-10-20 19:57:11 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/animecollection/main.go
@@ -0,0 +1,23 @@
+// cmd/animecollection/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "animecollection/internal/animecollection"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := animecollection.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
